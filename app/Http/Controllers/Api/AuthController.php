3462b93612be2a4c7e51ec04099c659676a6b39c
Bukhari_Inst <bukhari414647@gmail.com>
2023-09-25 22:22:43 +0700

    register

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use App\Models\Wallet;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+            'pin' => 'required|digits:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 400);
+        }
+
+        $user = User::where('email', $request->email)->exists();
+
+        if ($user) {
+            return response()->json(['message' => 'Email already taken'], 409);
+        }
+    }
+}
